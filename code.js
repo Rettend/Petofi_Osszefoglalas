2bb8df3483859294e26af4633a8176c63cbfbcdb
Rettend <hegyi.aron101@gmail.com>
2020-05-12 13:54:05 +0200

    asd

---
+++ code.js
@@ -0,0 +1,8 @@
+function Rainbow() {
+    document.getElementById("petofi").style.animationPlayState = "running";
+}
+
+function toggleActive(x) {
+    document.getElementById(x).classList.toggle("active");
+    
+}
