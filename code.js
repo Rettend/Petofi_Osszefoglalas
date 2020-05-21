def22d2d045672c568259ffb4a4c276e76a40b9a
Rettend <hegyi.aron101@gmail.com>
2020-05-21 19:09:14 +0200

    asd

--- code.js
+++ code.js
@@ -1,7 +1,13 @@
 function Rainbow() {
     document.getElementById("petofi").style.animationPlayState = "running";
+    document.getElementById("petofi").src = "petofiCool.png";
 }
 
 function toggleActive(x) {
     document.getElementById(x).classList.toggle("active");
+    if (document.body.style.overflowY === "scroll") {
+        document.body.style.overflowY = "hidden";
+    } else {
+        document.body.style.overflowY = "scroll";
+    }
 }
